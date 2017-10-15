f5cdceb51d75db0af78c9d8f8ffe122f12907124
Chrissy Hunt <chrissy.hunt@gmail.com>
2017-10-15 21:08:24 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -52,5 +52,13 @@ def sum_array(array)
 end
 
 def add_s(array)
-  
+  if
+
+  mod_array = array.map_with_index do |item, index|
+    if index == 1
+      item
+    else
+      item + "s"
+    end
+  end
 end
