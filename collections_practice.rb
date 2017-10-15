fb524c85bfcb6d9fd6d46f832def68ec697b5aed
Chrissy Hunt <chrissy.hunt@gmail.com>
2017-10-15 21:11:59 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -23,7 +23,11 @@ def swap_elements(array)
 end
 
 def swap_elements_from_to(array, index, destination_index)
-  
+  value_1 = array[index]
+  value_2 = array[destination_index]
+  array[index] = value_2
+  array[destination_index] = value_1
+  array
 end
 
 def reverse_array(array)
